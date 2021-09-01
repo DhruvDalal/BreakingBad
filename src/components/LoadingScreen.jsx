48d391b869f04626da530b367f6448c249f03a2a
DhruvDalal <dhruvdalal442002@gmail.com>
2021-09-01 11:52:04 +0530

    😂😂

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -8,7 +8,7 @@ const LoadingScreen = () => {
     return (
         <div className="screen">
             <img src={logo} alt="logo" className="logo"/>
-            <Loader type="Bars" color="red" height={80} width={80} />;
+            <Loader type="Bars" color="red" height={80} width={80} />
         </div>
     )
 }
